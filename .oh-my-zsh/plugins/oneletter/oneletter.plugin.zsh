e3e13a82239ff110f22a094cbb1157d2f42cff33
Jaroslaw Pendowski <jarek@pendowski.com>
2015-08-31 12:51:36 +0200

    Removed oh-my-zsh as submodule, added it as normal directory

---
+++ .oh-my-zsh/plugins/oneletter/oneletter.plugin.zsh
@@ -0,0 +1,62 @@
+# open Xcode workspace/project 
+
+function x() {
+	xcode_workspace=`ls | grep "\.xcworkspace"`
+	xcode_project=`ls | grep "\.xcproj"`
+	version="default"
+	
+	if [[ $1 == "beta" ]]; then
+		version="beta"
+	fi
+	
+	if [[ `echo $xcode_workspace | wc -l` -eq 1 ]]; then
+		xcode_open $version $xcode_workspace 
+		return 0
+	elif [[ `echo $xcode_workspace | wc -l` -ge 1  ]]; then
+		echo "Choose workspace:"
+		i=1
+		for workspace in ${(@f)$(ls | grep "\.xc")}; do
+			echo "$i. $workspace"
+			i=i+1
+		done
+		return 0
+	fi
+	
+	if [[ `echo $xcode_project | wc -l` == 1 ]]; then
+		xcode_open $version $xcode_project 
+		return 0
+	elif [[ `echo $xcode_project | wc -l` > 1  ]]; then
+		return 0
+	fi
+	
+	echo "No Xcode workspace or project found"
+}
+
+# open Xcode workspace/project in Xcode beta
+function xb() {
+	x "beta"
+}
+
+function afk() {
+	/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend && sleep 4 && pmset displaysleepnow
+}
+
+function xcode_open() {
+	xcode='/Applications/Xcode.app'
+	project=$2
+	if [[ $1 = "b" || $1 = "beta" ]]; then
+		xcode='/Applications/Xcode-beta.app'
+	fi
+	
+	echo "Opening $project"
+	open -a $xcode $project
+}
+
+# open selected/current directory in Finder
+function o() {
+	if [[ $# -eq 0 ]]; then
+		open .
+	else
+		open $*
+	fi
+}
